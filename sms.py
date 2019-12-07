efdad4db4993aa5b65074bb4cf0fd59967d804b0
Mohd. Baquir Qureshi <42351114+mohd-baquir-qureshi@users.noreply.github.com>
2019-12-07 19:47:15 +0530

    Create sms.py

---
+++ sms.py
@@ -0,0 +1,19 @@
+import requests
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Welcome to SMS Python Server"
+
+@app.route('/<sid>/<num>/<msg>')
+def msg(sid,num,msg):
+    smsUrl = 'http://api.msg91.com/api/sendhttp.php?route=4&sender=%s&mobiles=91%s&authkey={YOUR MSG91 AUTH KEY}&message=%s&country=91' % (sid,num,msg)
+    sendSms = requests.get(smsUrl)
+    balanceUrl = 'https://control.msg91.com/api/balance.php?authkey={YOUR MSG91 AUTH KEY}&type={1=>PROMOTIONAL, 2=>TRANSACTIONAL, 106=>OTP}'
+    remainingBalance = requests.get(balanceUrl).text
+    return remainingBalance
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0',port=5000,use_reloader=True)
