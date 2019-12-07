cb0cf4f61840a23322cc6f567b39becf8d5a0cd9
Mohd. Baquir Qureshi <42351114+mohd-baquir-qureshi@users.noreply.github.com>
2019-12-07 20:17:23 +0530

    Added authKey Variable

--- sms.py
+++ sms.py
@@ -8,9 +8,10 @@ def home():
 
 @app.route('/<sid>/<num>/<msg>')
 def msg(sid,num,msg):
-    smsUrl = 'http://api.msg91.com/api/sendhttp.php?route=4&sender=%s&mobiles=91%s&authkey={YOUR MSG91 AUTH KEY}&message=%s&country=91' % (sid,num,msg)
+    authKey = {YOUR MSG91 AUTH KEY}
+    smsUrl = 'http://api.msg91.com/api/sendhttp.php?route=4&sender=%s&mobiles=91%s&authkey=authKey&message=%s&country=91' % (sid,num,msg)
     sendSms = requests.get(smsUrl)
-    balanceUrl = 'https://control.msg91.com/api/balance.php?authkey={YOUR MSG91 AUTH KEY}&type={1=>PROMOTIONAL, 2=>TRANSACTIONAL, 106=>OTP}'
+    balanceUrl = 'https://control.msg91.com/api/balance.php?authkey=authKey&type={1=>PROMOTIONAL, 2=>TRANSACTIONAL, 106=>OTP}'
     remainingBalance = requests.get(balanceUrl).text
     return remainingBalance
 
